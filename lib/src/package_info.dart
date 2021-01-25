3ae6e5f5769e73c2e1428316ea680a88c994662d
Mahdi <mahdi3466@yahoo.com>
2021-01-25 19:26:00 +0330

    Format.

--- lib/src/package_info.dart
+++ lib/src/package_info.dart
@@ -7,8 +7,9 @@ class PackageInfo {
 
   factory PackageInfo.fromJson(Map<String, dynamic> json) {
     final actList = List.from(json['activities']);
-    final activities =
-        actList.map((e) => Map<String, dynamic>.from(e)).map((e) => ActivityInfo.fromJson(e));
+    final activities = actList
+        .map((e) => Map<String, dynamic>.from(e))
+        .map((e) => ActivityInfo.fromJson(e));
 
     return PackageInfo(activities: activities.toList());
   }
